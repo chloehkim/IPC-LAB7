af744a667e3b28a0fa6d876415317396606885ac
chloehkim <chloekim6156@gmail.com>
2025-03-11 17:13:28 +0000

    #

---
+++ class1.c
@@ -0,0 +1,36 @@
+/* Internal swap
+ *  internal_swap.c
+ */
+
+ #include <stdio.h>
+
+ void internal_swap (int a, int b)
+ {
+     int c;
+ 
+     printf("a is %d, b is %d\n", a, b);
+ 
+     c = a;
+     a = b;
+     b = c;
+ 
+     printf("a is %d, b is %d\n", a, b);
+ }
+ 
+ int main(void)
+ {
+     int a, b;
+ 
+     printf("a is ");
+     scanf("%d", &a);  //여기는 a,b를 이니셜라이즈 한다 인풋을 통해서
+     printf("b is ");
+     scanf("%d", &b);
+ 
+     internal_swap(a, b); // 
+ 
+     printf("After internal_swap:\n"
+             "a is %d\n"
+             "b is %d\n", a, b);
+ 
+     return 0;
+ }
